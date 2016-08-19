05c3f9614a83b0539cd9ea77e1d7110f24d9a1e0
marinazz <marinamontreal212@gmail.com>
2016-08-19 20:17:41 +0200

    try emoji position

--- script.js
+++ script.js
@@ -1,9 +1,9 @@
 jQuery(document).ready(function ($) {
         
-        $('.emoji:eq(0)').css('transform', 'translate(40px,20px) scale(5,5)')
-        $('.emoji:eq(1)').css('transform', 'translate(60px,10px) scale(5,5)')
-        $('.emoji:eq(2)').css('transform', 'translate(80px,20px) scale(5,5)')
-        $('.emoji:eq(3)').css('transform', 'translate(100px,10px) scale(5,5)')
+        $('.emoji:eq(0)').css('transform', 'translate(400px,200px) scale(5,5)')
+        $('.emoji:eq(1)').css('transform', 'translate(600px,100px) scale(5,5)')
+        $('.emoji:eq(2)').css('transform', 'translate(800px,200px) scale(5,5)')
+        $('.emoji:eq(3)').css('transform', 'translate(1000px,100px) scale(5,5)')
         
         var _SlideshowTransitions = [
         {$Duration:1200,x:0.3,$During:{$Left:[0.3,0.7]},$Easing:{$Left:$JssorEasing$.$EaseInCubic,$Opacity:$JssorEasing$.$EaseLinear},$Opacity:2}
